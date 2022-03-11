d9a420f3486ea3f79c4858f5773516a827ee4e06
Juaquin Andres <100443432+juakolp86@users.noreply.github.com>
2022-03-10 21:35:04 -0300

    Update enviar.php

--- enviar.php
+++ enviar.php
@@ -5,22 +5,21 @@ $mail = $_POST['email'];
 $phone = $_POST['number'];
 $message = $_POST['coment'];
 
-$header = 'From: ' . $mail . "\r\n";
-$header .= 'X-Mailer: PHP/' . phpversion() . "\r\n";
+$header = 'From: '.$mail."\r\n";
+$header .= 'X-Mailer: PHP/'.phpversion()."\r\n";
 $header .= "Mime-Version: 1.0 \r\n";
 $header .= "Coment-Type: text/plain";
 
-$message = "Este mensaje fue enviado por: " . $name . " \r\n";
-$message .= "Su mail es: " . $mail . " \r\n";
-$message .= "Telefono de contacto: " . $phone . " \r\n";
-$message .= "Mensaje: " . $_POST['coment'] . " \r\n";
-$message .= "Enviado el: " . date('d/m/Y', time());
+$message = "Este mensaje fue enviado por: ".$name." \r\n";
+$message .= "Su mail es: ".$mail." \r\n";
+$message .= "Telefono de contacto: ".$phone." \r\n";
+$message .= "Mensaje: ".$_POST['coment']." \r\n";
+$message .= "Enviado el: ".date('d/m/Y', time());
 
 $para = 'juaquingonzalez012@gmail.com';
 $asunto = 'Asunto del mensaje';
 
 mail($para, $asunto, utf8_decode($message), $header);
-echo "<script>alert('Correo enviado correctamente')</script>";
-echo "<script> setTimeout(\"location.href='index.html'\",1000)</script>";
+header("Location:index.html");
 }
 ?>
